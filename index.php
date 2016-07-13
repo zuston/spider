1ed96d21f3ed074c17c37f9f35a9fb54b48df7bd
zuston.shacha <zuston.shacha@gmail.com>
2016-07-13 16:48:56 +0800

    增加并行抓取，加快速度

--- index.php
+++ index.php
@@ -15,68 +15,70 @@ include './core/curl.class.php';
 include './core/regularExpression.function.php';
 include './db/db.php';
 
-while(1) {
+while (1) {
     $pdo = new db();
     $pdo->getInstance();
     $queue = $pdo->getQueue();
     $queueNickName = $queue[0]['nick_name'];
-
-//$pdo -> getQueueExsit('sha-ha-57');
+    $pdo -> updateQueueMark($queueNickName,2);
 
     $curlInstance = new curl($queueNickName, $config);
-//获取当前爬取用户信息,并且存入user表中
+    //获取当前爬取用户信息,并且存入user表中
     $spiderFolloweeContent = $curlInstance->robotSpider('followees');
     $spiderFollowerContent = $curlInstance->robotSpider('followers');
     $res = regularExpression::getCurrentUserInfo($spiderFolloweeContent);
     $res[] = $queueNickName;
     if ($pdo->saveUser($res)) {
-        echo "============{$res[0]}的个人信息已经录入======================================\n";
+        echo "============{$res[0]}的个人信息已经录入============\n";
     } else {
-        echo "============{$res[0]}的个人信息录入失败===========================\n";
+        echo "============{$res[0]}的个人信息录入失败============\n";
     }
-//var_dump($savaRes);exit;
 
 
     $hashId = $res[11];
     $followees = $res[9];
     $followers = $res[10];
 
-//为抓取用户关注表nick_name和被关注表的nick_name
+    //为抓取用户关注表nick_name和被关注表的nick_name
     if ($followees > 19) {
-        $queueSpiderContent = $curlInstance->robotSpider('followees', $res[9], $hashId);
+        $queueSpiderContent = $curlInstance->robotSpider('followees', $followees, $hashId);
         $followees = regularExpression::getOnePageByNumber($queueSpiderContent . $spiderFolloweeContent);
-//    var_dump($followees[1]);exit;
     } else {
         $followees = regularExpression::getOnePageByNumber($spiderFolloweeContent);
-//    var_dump($followees[1]);exit;
     }
 
+    echo "============{$res[0]}的应有{$res[9]}进入任务队列============\n";
     if ($pdo->saveQueue($followees[1])) {
-        echo "============{$res[0]}的followees已经进入任务队列中===============================\n";
+        echo "============{$res[0]}的followees已经进入任务队列中============\n";
     } else {
-        echo "============{$res[0]}的followees进入失败,怀疑为重复爬取===========================\n";
+        echo "============{$res[0]}的followees进入失败,怀疑为重复爬取============\n";
     }
 
-//var_dump($followers);exit;
     if ($followers > 19) {
-        $queueSpiderContent = $curlInstance->robotSpider('followers', $res[9], $hashId);
+        $queueSpiderContent = $curlInstance->robotSpider('followers', $followers, $hashId);
         $followers = regularExpression::getOnePageByNumber($queueSpiderContent . $spiderFolloweeContent);
     } else {
         $followers = regularExpression::getOnePageByNumber($spiderFollowerContent);
-//    var_dump($followers[1]);exit;
+    }
+
+    //存储用户关系
+    if($pdo->saveRelationT($queueNickName,$followees[1])){
+        echo "============{$res[0]}的关注的用户已经进入relation表============\n";
+    }else{
+        echo "============{$res[0]}的关注的用户进入relation表失败============\n";
     }
 
     if ($pdo->saveQueue($followers[1])) {
-        echo "============{$res[0]}的followers已经进入任务队列中===============================\n";
+        echo "============{$res[0]}的followers已经进入任务队列中============\n";
     } else {
-        echo "============{$res[0]}的followers进入失败,怀疑为重复爬取===========================\n";
+        echo "============{$res[0]}的followers进入失败,怀疑为重复爬取============\n";
     }
 
-//一次爬取结束,更新queue的当前nickName的mark值
+    //一次爬取结束,更新queue的当前nickName的mark值
     if ($pdo->updateQueueMark($queueNickName)) {
-        echo "============{$res[0]}的爬取成功结束=============================================\n";
+        echo "============{$res[0]}的爬取成功结束============\n\n";
     } else {
-        echo "============{$res[0]}的爬取失败结束=============================================\n";
+        echo "============{$res[0]}的爬取失败结束============\n\n";
 
     }
 
